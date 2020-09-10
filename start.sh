1d4baf4fceda0d9aaaf87ec6082c584781930352
IwamotoKosuke <iwamoto.dengana@gmail.com>
2020-09-10 19:42:49 +0900

    実験3

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-pip3 install -r requirements.txt &
+pip3 install --user -r requirements.txt &
 python3 blockchain_server.py &
 python3 blockchain_server_5001.py &
 python3 blockchain_server_5002.py &
