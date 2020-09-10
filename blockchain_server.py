b0087c4a1ddb17e34e44c2171cbecda2bfa1dce7
IwamotoKosuke <iwamoto.dengana@gmail.com>
2020-09-10 22:57:58 +0900

    実験7

--- blockchain_server.py
+++ blockchain_server.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 from flask import Flask
 from flask import jsonify
 from flask import request
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 sudo pip3 install --user -r requirements.txt &
-python3 blockchain_server.py
+python3 blockchain_server.py &
+python3 blockchain_server_5001.py &
+python3 blockchain_server_5002.py
