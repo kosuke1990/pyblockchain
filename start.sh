16a4b17294426df66e941971039c7a4ee702a568
IwamotoKosuke <iwamoto.dengana@gmail.com>
2020-09-10 20:21:55 +0900

    実験6

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
 sudo pip3 install --user -r requirements.txt &
-python3 blockchain_server.py &
-python3 blockchain_server_5001.py &
-python3 blockchain_server_5002.py &
-python3 wallet_server.py &
-python3 wallet_server_8081.py
+python3 blockchain_server.py
