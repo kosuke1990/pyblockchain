f03c24e5eef9b45c9cbdae8a1e0a488330b3e1a0
IwamotoKosuke <iwamoto.dengana@gmail.com>
2020-09-11 11:16:50 +0900

    微修正

--- wallet_server_8081.py
+++ wallet_server_8081.py
@@ -10,20 +10,23 @@ import wallet
 
 app = Flask(__name__, template_folder='./templates')
 
+
 @app.route('/')
 def index():
     return render_template('./index.html')
     
+
 @app.route('/wallet', methods=['POST'])
 def create_wallet():
     my_wallet = wallet.Wallet()
-    response={
-        'private_key':my_wallet.private_key,
-        'public_key':my_wallet.public_key,
-        'blockchain_address':my_wallet.blockchain_address,
+    response = {
+        'private_key': my_wallet.private_key,
+        'public_key': my_wallet.public_key,
+        'blockchain_address': my_wallet.blockchain_address,
     }
     return jsonify(response), 200
     
+
 @app.route('/transaction', methods=['POST'])
 def create_transaction():
     request_json = request.json
@@ -37,53 +40,54 @@ def create_transaction():
     if not all(k in request_json for k in required):
         return 'missing values', 400
     
-    sender_private_key=request_json['sender_private_key']
-    sender_blockchain_address=request_json['sender_blockchain_address']
-    recipient_blockchain_address=request_json['recipient_blockchain_address']
-    sender_public_key=request_json['sender_public_key']
-    value=float(request_json['value'])
+    sender_private_key = request_json['sender_private_key']
+    sender_blockchain_address = request_json['sender_blockchain_address']
+    recipient_blockchain_address = request_json['recipient_blockchain_address']
+    sender_public_key = request_json['sender_public_key']
+    value = float(request_json['value'])
     
-    transaction=wallet.Transaction(
+    transaction = wallet.Transaction(
         sender_private_key,
         sender_public_key,
         sender_blockchain_address,
         recipient_blockchain_address,
         value
         )
-    json_data={
+    json_data = {
         'sender_blockchain_address':sender_blockchain_address,
         'recipient_blockchain_address':recipient_blockchain_address,        
         'sender_public_key':sender_public_key,        
         'value':value,  
         'signature':transaction.generate_signature(),        
     }
+
     response = requests.post(
         urllib.parse.urljoin(app.config['gw'], 'transactions'),
         json=json_data, timeout=10)
     
-    if response.status_code ==201:
-        return jsonify({'message':'success'}), 201
-    return jsonify({'message':'fail', 'response':response}), 400
+    if response.status_code == 201:
+        return jsonify({'message': 'success'}), 201
+    return jsonify({'message': 'fail', 'response': response}), 400
     
     
 @app.route('/wallet/amount', methods=['GET'])
 def calculate_amount():
-    required=['blockchain_address']
+    required = ['blockchain_address']
     if not all(k in request.args for k in required):
         return 'Missing values', 400
     
-    my_blockchain_address=request.args.get('blockchain_address')
+    my_blockchain_address = request.args.get('blockchain_address')
     response = requests.get(
         urllib.parse.urljoin(app.config['gw'], 'amount'),
-        {'blockchain_address':my_blockchain_address},
+        {'blockchain_address': my_blockchain_address},
         timeout=5)
-    if response.status_code==200:
-        total=response.json()['amount']
-        return jsonify({'message':'success', 'amount':total}), 200
-    return jsonify({'message':'fail', 'error':response.content}), 400
+    if response.status_code == 200:
+        total = response.json()['amount']
+        return jsonify({'message': 'success', 'amount': total}), 200
+    return jsonify({'message': 'fail', 'error': response.content}), 400
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument('-p', '--port', default=8081,
@@ -94,5 +98,5 @@ if __name__=='__main__':
     port = args.port
     app.config['gw'] = args.gw
     
-    app.run(host='0.0.0.0',port=port, threaded=True,debug=True)
+    app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
     
